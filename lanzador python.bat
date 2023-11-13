368f88400f29d66768df9d28a9406d70695c8ab8
cmedinga <112566083+cmedinga@users.noreply.github.com>
2023-11-13 12:11:03 +0100

    Add files via upload

--- lanzador python.bat
+++ lanzador python.bat
@@ -1,4 +1,14 @@
 echo off
+set script="%cd%\nombre_de_tu_fichero_con_el_codigo_python.py"
+set CDIR=%cd%
+
+::Si existe el python instalado directamente, vamos por esa vía
+if exist "C:\Program Files\python39-32\python.exe" (
+   "C:\Program Files\python39-32\python.exe" %script%
+   goto finalizar
+)
+
+::Si no existe el python directamente, buscamos la instalación de Anaconda en las posibles carpetas
 if exist "C:\Program Files\Anaconda\python.exe" (
 	set pyexe="C:\Program Files\Anaconda\python.exe"
 	set pydir="C:\Program Files\Anaconda"
@@ -23,8 +33,7 @@ if exist "C:\Program Files\Anaconda\python.exe" (
 		)
 	)
 )
-%pyexe% %pydir%\cwp.py %pydir% %pyexe% "%cd%\script.py"
+%pyexe% %pydir%\cwp.py %pydir% %pyexe% %script%
 
+:finalizar
 pause
-
-
